a2e3027739c5dbcebaccd9a044e1cb5f61d4403d
Clyde <lkwowfs@163.com>
2019-06-12 15:52:22 +0800

    初始化 准备自己攻击自己

---
+++ src/main/java/com/example/demo/NoPatchForStupidApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoPatchForStupidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoPatchForStupidApplication.class, args);
+	}
+
+}
